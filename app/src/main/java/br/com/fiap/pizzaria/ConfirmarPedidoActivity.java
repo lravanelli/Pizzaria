a7ab03b20acb85ac72ed78e3e6cae82a55e9bc55
lravanelli <ravanelli.luis@gmail.com>
2017-12-13 19:25:59 -0200

    Passando Valor

--- app/src/main/java/br/com/fiap/pizzaria/ConfirmarPedidoActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/ConfirmarPedidoActivity.java
@@ -1,13 +1,25 @@
 package br.com.fiap.pizzaria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import br.com.fiap.pizzaria.model.Pedido;
 
 public class ConfirmarPedidoActivity extends AppCompatActivity {
 
+    Pedido pedido = new Pedido();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_pedido);
+
+        Intent i = getIntent();
+
+        pedido = i.getParcelableExtra("PEDIDO");
+
+        ((TextView)findViewById(R.id.tvTexto)).setText(pedido.getTamanho());
+
     }
 }
